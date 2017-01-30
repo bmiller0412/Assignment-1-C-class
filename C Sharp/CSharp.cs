d4530fe3ca8b1f257f4eb00a4eb8e386667414c8
Brandon <Brandon@BRANDON-PC>
2017-01-29 19:24:58 -0700

    Add project files.

---
+++ C Sharp/CSharp.cs
@@ -0,0 +1,34 @@
+﻿// Programing Homework 1
+// Developer: Brandon Miller
+// Date: 1/24/17
+// Purpose: Funs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    class CSharp
+    {
+        static void Main(string[] args)
+        {
+/* This code will display C # written with C's and #'s 
+   and change the background color to white and font (foreground) color to black */
+    
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("CCCCCCCCCCCC");
+            Console.WriteLine("CC                             ##  ##");
+            Console.WriteLine("CC                         ##############");
+            Console.WriteLine("CC                             ##  ##");
+            Console.WriteLine("CC                         ##############");
+            Console.WriteLine("CC                             ##  ##");
+            Console.WriteLine("CCCCCCCCCCCC");
+            Console.ReadLine();
+        }
+    }
+}
